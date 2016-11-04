ec3f71e862b94f408d4d65ae7205e5e90d89464d
Di Wang <wangdicoder@gmail.com>
2016-11-04 18:14:46 +1100

    sign-in page

---
+++ js/page/SignInAndSignup/Button.js
@@ -0,0 +1,34 @@
+/**
+ * Created by wangdi on 4/11/16.
+ */
+import React, {Component, PropTypes} from 'react';
+import {Text, View, StyleSheet, Platform, TouchableOpacity, TouchableNativeFeedback} from 'react-native';
+
+export default class Button extends Component{
+    static propTypes = {
+        text: PropTypes.string.isRequired,
+        onPress: PropTypes.func
+    };
+
+    render(){
+        if(Platform.OS === 'android') {
+            return (
+                <TouchableNativeFeedback
+                    onPress={this.props.onPress}>
+                    <View style={{flex: 1, height: 65, backgroundColor: '#046ada', alignItems:'center', justifyContent:'center'}}>
+                        <Text style={styles.text}>{this.props.text}</Text>
+                    </View>
+                </TouchableNativeFeedback>
+            );
+        }else if(Platform.OS === 'ios'){
+
+        }
+    }
+}
+
+const styles = StyleSheet.create({
+   text:{
+       color: 'white',
+       fontSize: 20
+   }
+});
--- js/page/SignInAndSignup/SignInPage.js
+++ js/page/SignInAndSignup/SignInPage.js
@@ -2,14 +2,47 @@
  * Created by wangdi on 4/11/16.
  */
 import React, {Component} from 'react';
-import {Text, View, StyleSheet, Platform} from 'react-native';
+import {Text, View, StyleSheet, Platform, TouchableOpacity, Image, TextInput} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import Button from './Button';
 
 export default class SignInPage extends Component{
+
+    _onPress(){
+
+    }
 
     render(){
         return(
             <View style={styles.view}>
-                <Text></Text>
+                <View style={styles.actionBar}>
+                    <TouchableOpacity onPress={this._onPress.bind(this)}>
+                        <Icon name="md-arrow-back" size={30} color="white"/>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.logo}>
+                    <Image source={require('../../image/ic_launcher.png')}/>
+                </View>
+                <View style={styles.editGroup}>
+                    <View style={styles.editView1}>
+                        <TextInput
+                            style={styles.edit}
+                            underlineColorAndroid="transparent"
+                            placeholder="手机号/邮箱"
+                            placeholderTextColor="#c4c4c4"/>
+                    </View>
+                    <View style={{height: 1, backgroundColor:'#c4c4c4'}}/>
+                    <View style={styles.editView2}>
+                        <TextInput
+                            style={styles.edit}
+                            underlineColorAndroid="transparent"
+                            placeholder="密码"
+                            placeholderTextColor="#c4c4c4"/>
+                    </View>
+                    <View style={{marginTop: 20}}>
+                        <Button text="登录" onPress={this._onPress.bind(this)}/>
+                    </View>
+                </View>
             </View>
         );
     }
@@ -20,5 +53,37 @@ const styles = StyleSheet.create({
         flex: 1,
         marginTop: (Platform.OS === 'ios') ? 20 : 0,
         backgroundColor: 'rgb(22,131,251)'
+    },
+    actionBar:{
+        margin: 15
+    },
+    logo:{
+        alignItems: 'center'
+    },
+    edit:{
+        height: 45,
+        fontSize: 20,
+        backgroundColor: '#fff',
+        marginLeft: 20,
+        marginRight: 20,
+        marginBottom: 7
+    },
+    editView1:{
+        height: 65,
+        backgroundColor:'white',
+        justifyContent: 'flex-end',
+        borderTopLeftRadius: 3,
+        borderTopRightRadius: 3
+    },
+    editView2:{
+        height: 65,
+        backgroundColor:'white',
+        justifyContent: 'flex-end',
+        borderBottomLeftRadius: 3,
+        borderBottomRightRadius: 3
+    },
+    editGroup:{
+        margin: 20
     }
+
 });
