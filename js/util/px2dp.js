2ef8bdf24365ee6edaac726af223b2a41af4df75
Di Wang <wangdicoder@gmail.com>
2016-11-05 22:05:27 +1100

    px2dp

--- js/util/px2dp.js
+++ js/util/px2dp.js
@@ -6,12 +6,15 @@
 import {Dimensions} from 'react-native';
 
 // 58 app 只有竖屏模式，所以可以只获取一次 width
-const deviceWidthDp = Dimensions.get('window').width;
+//const deviceWidthDp = Dimensions.get('window').width;
+const deviceHeightDp = Dimensions.get('window').height;
 // UI 默认给图是 640
-const uiWidthPx = 640;
+const uiHeightPx = 640;
 
-function pxToDp(uiElementPx) {
-    return uiElementPx *  deviceWidthDp / uiWidthPx;
+function px2dp(uiElementPx) {
+    //console.log(deviceWidthDp);
+    //console.log(deviceHeightDp);
+    return uiElementPx *  deviceHeightDp / uiHeightPx;
 }
 
-export default pxToDp;
+export default px2dp;
