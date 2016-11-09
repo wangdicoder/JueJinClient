42c35cd78e2b4b55ba4988116fee2be428054ff1
Di Wang <wangdicoder@gmail.com>
2016-11-09 23:27:17 +1100

    list

---
+++ js/component/ListViewItem.js
@@ -0,0 +1,15 @@
+/**
+ * Created by wangdi on 9/11/16.
+ */
+'use strict';
+
+import React, {Component, PropTypes} from 'react';
+import ReactNative, {Text, View, StyleSheet, Platform, TouchableOpacity, TouchableNativeFeedback, Image} from 'react-native';
+import px2dp from '../util/px2dp';
+
+export default class ListViewItem extends Component{
+
+    render(){
+
+    }
+}
